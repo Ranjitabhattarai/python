c15b31438c35a5fb17a9d54a8e94a2c921cd9b38
Ranjitabhattarai <ranjitaranju816@gmail.com>
2026-05-22 07:55:35 +0500

    third program

--- list.py
+++ list.py
@@ -1,5 +1,16 @@
-mylist =list(("ranjita" , "rubina" , "Anjila" , "anjee" , "smriti"))
+mylist =list(("10" , "20" , "30" , "40" , "50"))
 print(mylist[0])
 print(mylist[-2])
 
 print(mylist[1:4])
+print(mylist[::-3])
+
+tuple1 = ("A" , "B" , "C" , "D" , "E")
+tuple2 = (1, 2, 3, 4, 5)
+tuple3 = ("true", "true", "false")
+
+
+
+print(tuple1)
+print(tuple2)
+print(tuple3)
