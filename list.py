38e0ccf1ddca2c2f401795a343c675f04026bc2c
Ranjitabhattarai <ranjitaranju816@gmail.com>
2026-06-04 18:53:24 +0500

    commit

--- list.py
+++ list.py
@@ -23,4 +23,12 @@ pokhara = {
     "university" : "pokhara university"
 }
 
+
+pokhara["university"] = "kathmandu university"
 print(pokhara["university"])
+
+
+#to print list into tuples
+my_list = [1, 2, 3, 4, 5]
+my_tuple = tuple(my_list)
+print(my_tuple)
