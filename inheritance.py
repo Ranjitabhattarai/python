afce601dee111a6391eb0c42c8a4d1dadbb17dda
Ranjitabhattarai <ranjitaranju816@gmail.com>
2026-06-05 07:25:11 +0500

    commit

--- inheritance.py
+++ inheritance.py
@@ -1,27 +1,61 @@
-# Parent Class
+# Parent class
 class Parent:
     def __init__(self, father_name, mother_name):
-        self.father_name = father_name
-        self.mother_name = mother_name
+        self.__father_name = father_name   # private attribute
+        self.__mother_name = mother_name   # private attribute
 
-    def display_parent_details(self):
-        print("Father Name:", self.father_name)
-        print("Mother Name:", self.mother_name)
+    # Getter methods
+    def get_father_name(self):
+        return self.__father_name
+
+    def get_mother_name(self):
+        return self.__mother_name
+
+    # Setter methods
+    def set_father_name(self, father_name):
+        self.__father_name = father_name
+
+    def set_mother_name(self, mother_name):
+        self.__mother_name = mother_name
 
 
-# Child Class (inherits Parent)
+# Child class inheriting Parent
 class Student(Parent):
-    def __init__(self, student_name, father_name, mother_name):
-        super().__init__(father_name, mother_name)  # Initialize Parent class
-        self.student_name = student_name
+    def __init__(self, student_name, roll_no, father_name, mother_name):
+        super().__init__(father_name, mother_name)
+        self.__student_name = student_name
+        self.__roll_no = roll_no
 
-    def display_student_details(self):
-        print("Student Name:", self.student_name)
-        self.display_parent_details()
+    # Getter methods
+    def get_student_name(self):
+        return self.__student_name
+
+    def get_roll_no(self):
+        return self.__roll_no
+
+    # Setter methods
+    def set_student_name(self, student_name):
+        self.__student_name = student_name
+
+    def set_roll_no(self, roll_no):
+        self.__roll_no = roll_no
+
+    def display_details(self):
+        print("Student Name:", self.get_student_name())
+        print("Roll No:", self.get_roll_no())
+        print("Father's Name:", self.get_father_name())
+        print("Mother's Name:", self.get_mother_name())
 
 
-# Creating Object
-s1 = Student("Ranjita", "indra bhattarai", "bishnu battarai")
+# Creating object
+s1 = Student("Ram", 101, "Hari Prasad", "Sita Devi")
 
-# Display Details
-s1.display_student_details()
+# Display details
+s1.display_details()
+
+# Modifying data using setter methods
+s1.set_student_name("Shyam")
+s1.set_roll_no(102)
+
+print("\nAfter Updating:")
+s1.display_details()
